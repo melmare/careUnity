5113928cd2595bd9738682f8ebdc608daa528a6e
melmare <48948850+melmare@users.noreply.github.com>
2019-05-21 16:18:02 +0200

    Delete TestComponent

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,8 +1,4 @@
 export const imports = {
-  'src/TestComponent.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "src-test-component" */ 'src/TestComponent.mdx'
-    ),
   'src/newspage/Entry.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-newspage-entry" */ 'src/newspage/Entry.mdx'
--- src/TestComponent.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const TestComponent = styled.h2`
-  background: gray;
-`;
-
-export default TestComponent;
